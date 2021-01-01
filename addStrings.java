93e213a7a71ea3435138d46a80afdd35d4f62ec8
kyleyiyang <kyleyiyang@gmail.com>
2021-01-01 16:21:55 -0500

    Update addStrings.java

--- addStrings.java
+++ addStrings.java
@@ -1,1 +1,23 @@
-gg
+class Solution {
+    public String addStrings(String num1, String num2) {
+        String ans = "";
+        String s = num1;
+        String l = num2;
+        if (num1.length()>=num2.length()) {
+            l = num1;
+            s = num2;
+        }
+        int sum=0;
+        for (int i=0;i<l.length();i++) {
+            if (i<s.length()) {
+                sum += ((l.charAt(l.length()-i-1) - '0') + (s.charAt(s.length()-i-1) - '0')) * (Math.pow(10,i));
+                System.out.println(sum);
+            } else {
+                sum += ((l.charAt(l.length()-i-1) - '0') * (Math.pow(10,i)));
+                System.out.println("i="+i+";"+String.valueOf(((l.charAt(l.length()-i-1) - '0') * (Math.pow(10,i)))));
+                System.out.println("sum="+sum);
+            }
+        }
+        return String.valueOf(sum);
+    }
+}
